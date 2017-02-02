a70123d6ef8e615e98023bcc91205c6ee256c087
Em01 <emma_williams1@hotmail.co.uk>
2017-02-02 18:39:38 +0000

    Clear form spinner

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -18,11 +18,27 @@ class LoginForm extends Component {
     this.setState({ error: '', loading: true})
 
     firebase.auth().signInWithEmailAndPassword(email, password)
+    .then(this.onLoginSuccess.bind(this))
     .catch(() => {
       firebase.auth().createUserWithEmailAndPassword(email, password)
-        .catch(() => {
-          this.setState({ error: 'Authentication Failed' })
-        })
+        .then(this.onLoginSuccess.bind(this))
+        .catch(this.onLoginFail.bind(this))
+    })
+  }
+
+  onLoginSuccess() {
+    this.setState({
+      email: '',
+      password: '',
+      loading: false,
+      error: ''
+    })
+  }
+
+  onLoginFail() {
+    this.setState({
+      error: 'Authentication Failed',
+      loading: false
     })
   }
 
