f6b74a61a993c907cb0c6e67d5218f4db65cbcef
Em01 <emma_williams1@hotmail.co.uk>
2017-02-02 16:35:58 +0000

    text input state

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -3,11 +3,17 @@ import { TextInput } from 'react-native'
 import { Button, Card, CardSection } from './common'
 
 class LoginForm extends Component {
+  state = { text: '' }
+
   render() {
     return (
       <Card>
         <CardSection>
-          <TextInput style={{ height: 20, width: 100}} />
+          <TextInput
+            value={this.state.text}
+            onChangeText={text => this.setState({ text })}
+            style={{ height: 20, width: 100}}
+          />
         </CardSection>
         <CardSection />
 
